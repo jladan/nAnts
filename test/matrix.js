96817144061c6619cbef5ffe64aa1cb5225b7bc2
John Ladan <john@ladan.ca>
2014-11-07 00:39:58 -0500

    Add some tests for matrix algebra

---
+++ test/matrix.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var algebra = require('../algebra.js');
+
+describe('Matrix', function () {
+    describe('#Creation', function () {
+
+        it('should use the provided array as a buffer', function () {
+            var a = [1,2,3,4];
+            var m = new algebra.Matrix([2,2], a);
+            m.array.should.equal(a);
+        });
+            
+        it('should copy the size property', function () {
+            var size = [2,4];
+            var m = new algebra.Matrix(size);
+            m.size.should.not.equal(size);
+        });
+
+        it('should have the correct size', function () {
+            var size = [2,4];
+            var m = new algebra.Matrix(size);
+            m.size.should.eql(size);
+        });
+    });
+});
