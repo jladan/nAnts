7818bd8fa6025f5318b12b09a7ca163e13c2451b
John Ladan <john@ladan.ca>
2016-08-21 22:16:48 -0400

    change the module structure to the current TS/ecmascript2015 style

--- src/ode.ts
+++ src/ode.ts
@@ -1,126 +1,123 @@
+/* forward euler solver to test design
+    * 
+    * The dimension `d` is determined from the `initial` argument
+    * derivative is a function(Array x, float t, Array parameters) that
+    *              calculates the derivative for the ODE model
+    * initial is a d-dimensional array defining the initial condition
+    * dt is the time-step
+    * t_final is the total time
+    * parameters is an array of the parameters for the ODE being solved
+    */
+export function forward_euler(derivative, initial, dt, t_final, parameters) {
+    var d = initial.length;
+    var N = Math.floor(t_final / dt);
+    var result = new Float32Array(N * d);
+    var t = new Float32Array(N);
 
-module ode {
-    /* forward euler solver to test design
-     * 
-     * The dimension `d` is determined from the `initial` argument
-     * derivative is a function(Array x, float t, Array parameters) that
-     *              calculates the derivative for the ODE model
-     * initial is a d-dimensional array defining the initial condition
-     * dt is the time-step
-     * t_final is the total time
-     * parameters is an array of the parameters for the ODE being solved
-     */
-    function forward_euler(derivative, initial, dt, t_final, parameters) {
-        var d = initial.length;
-        var N = Math.floor(t_final / dt);
-        var result = new Float32Array(N * d);
-        var t = new Float32Array(N);
-
-        // initial conditions
-        var i, j;
-        var x_cur = initial;
-        for (j = 0; j < d; j++) result[j * N] = initial[j];
-        // the grunt-work!
-        for (i = 0; i < N-1; i++) {
-            var x_prime = derivative(x_cur, t[i], parameters)
-            t[i+1] = (i+1) * dt;
-            for (j = 0; j < d; j++) 
-                result[i + 1 + j * N] = x_cur[j] = x_cur[j] + dt * x_prime[j];
-        }
-        return [t, result, N];
-    }
-
-    function leap_frog(derivative, initial, dt, t_final, parameters) {
-        var d = initial.length;
-        var N = Math.floor(t_final / dt);
-        var result = new Float32Array(N * d);
-        var t = new Float32Array(N);
-
-        // initial conditions
-        var i, j;
-        for (j = 0; j < d; j++) result[j * N] = initial[j];
-        // Forward Euler for the first time-step
-        var x_old = initial.slice();
-        var x_cur = x_old.slice();
-        var x_prime = derivative(x_old, 0, parameters);
-        t[1] = dt;
+    // initial conditions
+    var i, j;
+    var x_cur = initial;
+    for (j = 0; j < d; j++) result[j * N] = initial[j];
+    // the grunt-work!
+    for (i = 0; i < N-1; i++) {
+        var x_prime = derivative(x_cur, t[i], parameters)
+        t[i+1] = (i+1) * dt;
         for (j = 0; j < d; j++) 
-            result[1 + j * N] = x_cur[j] = x_old[j] + dt * x_prime[j];
-
-        // the grunt-work!
-        var x_new = x_cur.slice();
-        for (i = 1; i < N-1; i++) {
-            x_prime = derivative(x_cur, t[i], parameters)
-            t[i+1] = (i+1) * dt;
-            for (j = 0; j < d; j++)  {
-                result[i + 1 + j * N] = x_new[j] = x_old[j] + 2 * dt * x_prime[j];
-                x_old[j] = x_cur[j];
-                x_cur[j] = x_new[j];
-            }
-        }
-        return [t, result, N];
+            result[i + 1 + j * N] = x_cur[j] = x_cur[j] + dt * x_prime[j];
     }
-    
-    function ab2 (derivative, initial, dt, t_final, parameters) {
-        var d = initial.length;
-        var N = Math.floor(t_final / dt);
-        var result = new Float32Array(N * d);
-        var t = new Float32Array(N);
+    return [t, result, N];
+}
 
-        // initial conditions
-        var i, j;
-        for (j = 0; j < d; j++) result[j * N] = initial[j];
-        // Forward Euler for the first time-step
-        var x_old = initial.slice();
-        var x_cur = x_old.slice();
-        var x_prime_old = derivative(x_old, 0, parameters);
-        t[1] = dt;
-        for (j = 0; j < d; j++) 
-            result[1 + j * N] = x_cur[j] = x_old[j] + dt * x_prime_old[j];
+export function leap_frog(derivative, initial, dt, t_final, parameters) {
+    var d = initial.length;
+    var N = Math.floor(t_final / dt);
+    var result = new Float32Array(N * d);
+    var t = new Float32Array(N);
 
-        // the grunt-work!
-        var x_new = x_cur.slice();
-        var x_prime_cur = x_prime_old.slice();
-        for (i = 1; i < N-1; i++) {
-            x_prime_cur = derivative(x_cur, t[i], parameters)
-            t[i+1] = (i+1) * dt;
-            for (j = 0; j < d; j++) {
-                result[i + 1 + j * N] = x_new[j] =
-                    x_cur[j] + dt*(3* x_prime_cur[j] - x_prime_old[j])/2;
-                x_old[j] = x_cur[j];
-                x_cur[j] = x_new[j];
-                x_prime_old[j] = x_prime_cur[j];
-            }
+    // initial conditions
+    var i, j;
+    for (j = 0; j < d; j++) result[j * N] = initial[j];
+    // Forward Euler for the first time-step
+    var x_old = initial.slice();
+    var x_cur = x_old.slice();
+    var x_prime = derivative(x_old, 0, parameters);
+    t[1] = dt;
+    for (j = 0; j < d; j++) 
+        result[1 + j * N] = x_cur[j] = x_old[j] + dt * x_prime[j];
+
+    // the grunt-work!
+    var x_new = x_cur.slice();
+    for (i = 1; i < N-1; i++) {
+        x_prime = derivative(x_cur, t[i], parameters)
+        t[i+1] = (i+1) * dt;
+        for (j = 0; j < d; j++)  {
+            result[i + 1 + j * N] = x_new[j] = x_old[j] + 2 * dt * x_prime[j];
+            x_old[j] = x_cur[j];
+            x_cur[j] = x_new[j];
         }
-        return [t, result, N];
     }
+    return [t, result, N];
+}
 
-    function heun (derivative, initial, dt, t_final, parameters) {
-        var d = initial.length;
-        var N = Math.floor(t_final / dt);
-        var result = new Float32Array(N * d);
-        var t = new Float32Array(N);
+export function ab2 (derivative, initial, dt, t_final, parameters) {
+    var d = initial.length;
+    var N = Math.floor(t_final / dt);
+    var result = new Float32Array(N * d);
+    var t = new Float32Array(N);
 
-        // initial conditions
-        var i, j;
-        var x_cur = initial.slice();
-        for (j = 0; j < d; j++) result[j * N] = x_cur[j];
-        var x_new=x_cur.slice(), x_next=x_cur.slice();
-        // the grunt-work!
-        for (i = 0; i < N-1; i++) {
-            t[i+1] = (i+1) * dt;
-            var x_prime_cur = derivative(x_cur, t[i], parameters);
+    // initial conditions
+    var i, j;
+    for (j = 0; j < d; j++) result[j * N] = initial[j];
+    // Forward Euler for the first time-step
+    var x_old = initial.slice();
+    var x_cur = x_old.slice();
+    var x_prime_old = derivative(x_old, 0, parameters);
+    t[1] = dt;
+    for (j = 0; j < d; j++) 
+        result[1 + j * N] = x_cur[j] = x_old[j] + dt * x_prime_old[j];
 
-            for (j=0; j<d; j++) 
-                x_next[j] = x_cur[j] + dt*x_prime_cur[j];
-            var x_prime_next = derivative(x_next, t[i+1], parameters);
-
-            for (j = 0; j < d; j++) {
-                result[i + 1 + j * N] = x_new[j] =
-                    x_cur[j] + dt * ( x_prime_cur[j] + x_prime_next[j])/2;
-                x_cur[j] = x_new[j];
-            }
+    // the grunt-work!
+    var x_new = x_cur.slice();
+    var x_prime_cur = x_prime_old.slice();
+    for (i = 1; i < N-1; i++) {
+        x_prime_cur = derivative(x_cur, t[i], parameters)
+        t[i+1] = (i+1) * dt;
+        for (j = 0; j < d; j++) {
+            result[i + 1 + j * N] = x_new[j] =
+                x_cur[j] + dt*(3* x_prime_cur[j] - x_prime_old[j])/2;
+            x_old[j] = x_cur[j];
+            x_cur[j] = x_new[j];
+            x_prime_old[j] = x_prime_cur[j];
         }
-        return [t, result, N];
     }
+    return [t, result, N];
+}
+
+export function heun (derivative, initial, dt, t_final, parameters) {
+    var d = initial.length;
+    var N = Math.floor(t_final / dt);
+    var result = new Float32Array(N * d);
+    var t = new Float32Array(N);
+
+    // initial conditions
+    var i, j;
+    var x_cur = initial.slice();
+    for (j = 0; j < d; j++) result[j * N] = x_cur[j];
+    var x_new=x_cur.slice(), x_next=x_cur.slice();
+    // the grunt-work!
+    for (i = 0; i < N-1; i++) {
+        t[i+1] = (i+1) * dt;
+        var x_prime_cur = derivative(x_cur, t[i], parameters);
+
+        for (j=0; j<d; j++) 
+            x_next[j] = x_cur[j] + dt*x_prime_cur[j];
+        var x_prime_next = derivative(x_next, t[i+1], parameters);
+
+        for (j = 0; j < d; j++) {
+            result[i + 1 + j * N] = x_new[j] =
+                x_cur[j] + dt * ( x_prime_cur[j] + x_prime_next[j])/2;
+            x_cur[j] = x_new[j];
+        }
+    }
+    return [t, result, N];
 }
--- src/stochastics.ts
+++ src/stochastics.ts
@@ -1,258 +1,254 @@
 /** nAnts stochastic simulation module
  */
 
-module stochastics {
+export enum Method {Euler, Milstein, Colour};
+export class Solution {
+    t: Float32Array | Float64Array;
+    result: Float32Array | Float64Array;
+    N: number;
 
-    export enum Method {Euler, Milstein, Colour};
-    export class Solution {
-        t: Float32Array | Float64Array;
-        result: Float32Array | Float64Array;
-        N: number;
-
-        constructor(t, result, n) {
-            this.t = t;
-            this.N = n;
-            this.result = result;
-        }
-
-        // Helper functions
-        getDimension(d: number): Float32Array | Float64Array {
-            return this.result.subarray(d * this.N,(d + 1) * this.N);
-        }
-
-        /** Extracts a 'trail' from the solution: pairs of (x,t)
-         * d is the index of the dimension (starting from 0)
-         * outStep is the number of time-steps between samples. Default to 1
-         */
-        getTrail(d: number, outStep?: number) {
-            outStep = outStep || 1;
-            var x = this.getDimension(d);
-            var i: number;
-            var result: Array<[number,number]> = new Array();
-            for (i = 0; i < this.N; i+=outStep)
-                result.push([this.t[i], x[i]]);
-            return result;
-        }
-
-        /** Extracts a trail in phase-space, coordinate (x_1, x_2)
-         * d1, and d2  are the indices of the coordinates (starting from 0)
-         * outStep is the number of time-steps between samples. Default to 1
-         */
-        getPhase(d1: number, d2: number, outStep?: number) {
-            outStep = outStep || 1;
-            var x = this.getDimension(d1);
-            var y = this.getDimension(d2);
-            var i: number;
-            var result: Array<[number,number]> = new Array();
-            for (i = 0; i < this.N; i+=outStep)
-                result.push([x[i], y[i]]);
-            return result;
-        }
-
+    constructor(t, result, n) {
+        this.t = t;
+        this.N = n;
+        this.result = result;
     }
 
-    /** Euler-Maruyama method
-     *
-     * Numerically simulates the Langevin equation,
-     *
-     *      dy/dt = A(y,t) + D(y,t) * noise(t)
-     *
-     * For Gaussian white noise. The equation is actually interpreted in
-     * the Ito sense.
-     *
-     * Arguments:
-     * A - function of (y,t,[c]) provides the non-stochastic rates
-     * D - function of (y,t,[c]) provides the stochastic rates
-     * parameters - array of parameters to be sent to the functions A,D
-     * the rest should be self-explanatory
-     */
-    export function euler(A: (x: number[], t: number, p: number[]) => number[],
-                          D: (x: number[], t: number, p: number[]) => number[],
-                          initial: number[], dt: number, t_final: number,
-                          pA: number[], pD: number[]): Solution {
-        var d: number = initial.length;
-        var N: number = Math.floor(t_final / dt);
-        var result = new Float32Array(N * d);
-        var t = new Float32Array(N);
-        var n: number; // the most random number
-        var sdt: number = Math.sqrt(dt);
-
-        // initial conditions
-        var i: number, j: number;
-        var y_cur: number[] = initial.slice();
-        for (j = 0; j < d; j++) result[j * N] = initial[j];
-        // the grunt-work!
-        for (i = 0; i < N - 1; i++) {
-            var A_cur = A(y_cur, t[i], pA)
-            var D_cur = D(y_cur, t[i], pD)
-            t[i + 1] = (i + 1) * dt;
-            for (j = 0; j < d; j++) {
-                n = gaussian();
-                result[i + 1 + j * N] = y_cur[j] =
-                    y_cur[j] + dt * A_cur[j] +
-                    n * D_cur[j] * sdt;
-            }
-        }
-        return new Solution(t, result, N);
+    // Helper functions
+    getDimension(d: number): Float32Array | Float64Array {
+        return this.result.subarray(d * this.N,(d + 1) * this.N);
     }
 
-    /** Milstein method
-     *
-     * Numerically simulates the Langevin equation,
-     *
-     *      dy/dt = A(y,t) + D(y,t) * noise(t)
-     *
-     * For Gaussian white noise. The equation is actually interpreted in
-     * the Ito sense.
-     *
-     * Arguments:
-     * A - function of (y,t,[c]) provides the non-stochastic rates
-     * D - function of (y,t,[c]) provides the stochastic rates
-     * Dy - derivative of D in y
-     * parameters - array of parameters to be sent to the functions A,D
-     * the rest should be self-explanatory
+    /** Extracts a 'trail' from the solution: pairs of (x,t)
+     * d is the index of the dimension (starting from 0)
+     * outStep is the number of time-steps between samples. Default to 1
      */
-    export function milstein(A: (x: number[], t: number, p: number[]) => number[],
-                             D: (x: number[], t: number, p: number[]) => number[],
-                             Dy: (x: number[], t: number, p: number[]) => number[],
-                             initial: number[], dt: number, t_final: number,
-                             pA: number[], pD: number[]): Solution {
-        var d: number = initial.length;
-        var N: number = Math.floor(t_final / dt);
-        var result = new Float32Array(N * d);
-        var t = new Float32Array(N);
-        var sdt: number = Math.sqrt(dt);
-
-        // initial conditions
-        var i: number, j: number, n: number[];
-        var y_cur: number[] = initial.slice();
-        for (j = 0; j < d; j++) result[j * N] = initial[j];
-        // the grunt-work!
-        for (i = 0; i < N - 1; i++) {
-            var A_cur = A(y_cur, t[i], pA)
-            var D_cur = D(y_cur, t[i], pD)
-            var Dy_cur = Dy(y_cur, t[i], pD)
-            t[i + 1] = (i + 1) * dt;
-            for (j = 0; j < d; j++) {
-                n = boxMuller2();
-                result[i + 1 + j * N] = y_cur[j] =
-                    y_cur[j] + dt * A_cur[j] + n[0] * D_cur[j] * sdt
-                    - dt / 2 * D_cur[j] * Dy_cur[j] * (1 - n[1] * n[1]);
-            }
-        }
-        return new Solution(t, result, N);
+    getTrail(d: number, outStep?: number) {
+        outStep = outStep || 1;
+        var x = this.getDimension(d);
+        var i: number;
+        var result: Array<[number,number]> = new Array();
+        for (i = 0; i < this.N; i+=outStep)
+            result.push([this.t[i], x[i]]);
+        return result;
     }
 
-    /** Coloured Noise method
-     *
-     * Numerically simulates the Langevin equation,
-     *
-     *      dy/dt = A(y,t) + D(y,t) * noise(t)
-     *
-     * where the noise term is an Ornstein-Uhlenbeck process,
-     * with autocorrelation:
-     *
-     *      <F(t)F(t-tau)> = sigma^2 exp(-|tau|/tau_c) .
-     *
-     * tau_c being the correlation time.
-     *
-     * Because the SDE is differentiable with coloured noise, the
-     * Heun method is used to advance each time-step.
-     *
-     * Arguments:
-     * A - function of (y,t,[c]) provides the non-stochastic rates
-     * D - function of (y,t,[c]) provides the stochastic rates
-     * sigma - standard deviation of gaussian noise
-     * tau - correlation time of the noise
-     * parameters - array of parameters to be sent to the functions A,D
-     * the rest should be self-explanatory
+    /** Extracts a trail in phase-space, coordinate (x_1, x_2)
+     * d1, and d2  are the indices of the coordinates (starting from 0)
+     * outStep is the number of time-steps between samples. Default to 1
      */
-    export function colour(A: (x: number[], t: number, p: number[]) => number[],
-                           D: (x: number[], t: number, p: number[]) => number[],
-                           sigma: number, tau: number,
-                           initial: number[], dt: number, t_final: number,
-                           pA: number[], pD: number[]): Solution {
-        var d: number = initial.length;
-        var N: number = Math.floor(t_final / dt);
-        var result = new Float32Array(N * d);
-        var t = new Float32Array(N);
-        var n: number = 4; // the most random number
+    getPhase(d1: number, d2: number, outStep?: number) {
+        outStep = outStep || 1;
+        var x = this.getDimension(d1);
+        var y = this.getDimension(d2);
+        var i: number;
+        var result: Array<[number,number]> = new Array();
+        for (i = 0; i < this.N; i+=outStep)
+            result.push([x[i], y[i]]);
+        return result;
+    }
 
-        // coefficients to advance noise
-        var rho: number = Math.exp(-dt / tau);
-        var rhoc: number = sigma * Math.sqrt(1 - rho * rho); // XXX why sigma and not previous
+}
 
-        // Required variables
-        var i: number, j: number;
-        var k1 = new Array(d), k2 = new Array(d);
-        var noiseCur = new Array(d), noiseNext = new Array(d);
+/** Euler-Maruyama method
+ *
+ * Numerically simulates the Langevin equation,
+ *
+ *      dy/dt = A(y,t) + D(y,t) * noise(t)
+ *
+ * For Gaussian white noise. The equation is actually interpreted in
+ * the Ito sense.
+ *
+ * Arguments:
+ * A - function of (y,t,[c]) provides the non-stochastic rates
+ * D - function of (y,t,[c]) provides the stochastic rates
+ * parameters - array of parameters to be sent to the functions A,D
+ * the rest should be self-explanatory
+ */
+export function euler(A: (x: number[], t: number, p: number[]) => number[],
+                        D: (x: number[], t: number, p: number[]) => number[],
+                        initial: number[], dt: number, t_final: number,
+                        pA: number[], pD: number[]): Solution {
+    var d: number = initial.length;
+    var N: number = Math.floor(t_final / dt);
+    var result = new Float32Array(N * d);
+    var t = new Float32Array(N);
+    var n: number; // the most random number
+    var sdt: number = Math.sqrt(dt);
 
-        // Initial Conditions
-        var y_cur: number[] = initial.slice();
+    // initial conditions
+    var i: number, j: number;
+    var y_cur: number[] = initial.slice();
+    for (j = 0; j < d; j++) result[j * N] = initial[j];
+    // the grunt-work!
+    for (i = 0; i < N - 1; i++) {
+        var A_cur = A(y_cur, t[i], pA)
+        var D_cur = D(y_cur, t[i], pD)
+        t[i + 1] = (i + 1) * dt;
         for (j = 0; j < d; j++) {
-            result[j * N] = initial[j];
-            noiseNext[j] = 0;
+            n = gaussian();
+            result[i + 1 + j * N] = y_cur[j] =
+                y_cur[j] + dt * A_cur[j] +
+                n * D_cur[j] * sdt;
         }
-        var y_next = y_cur.slice();
-        for (i = 0; i < N - 1; i++) {
-            // advance the noise and time
-            t[i + 1] = (i + 1) * dt;
-            for (j=0; j<d; j++) {
-                noiseCur[j] = noiseNext[j];
-                n = boxMuller();
-                noiseNext[j] = noiseCur[j] * rho + n * rhoc;
-            }
-            // use heun's method to advance the system
-            var A_cur = A(y_cur, t[i], pA)
-            var D_cur = D(y_cur, t[i], pD)
-            for (j = 0; j < d; j++) {
-                k1[j] = A_cur[j] + D_cur[j] * noiseCur[j];
-                y_next[j] = y_cur[j] + dt * k1[j];
-            }
-            var A_next = A(y_next, t[i], pA)
-            var D_next = D(y_next, t[i], pD)
-            for (j = 0; j < d; j++) {
-                k2[j] = A_next[j] + D_next[j] * noiseNext[j];
-                result[i + 1 + j * N] = y_cur[j] = y_cur[j] + dt / 2 * (k1[j] + k2[j]);
-            }
+    }
+    return new Solution(t, result, N);
+}
+
+/** Milstein method
+ *
+ * Numerically simulates the Langevin equation,
+ *
+ *      dy/dt = A(y,t) + D(y,t) * noise(t)
+ *
+ * For Gaussian white noise. The equation is actually interpreted in
+ * the Ito sense.
+ *
+ * Arguments:
+ * A - function of (y,t,[c]) provides the non-stochastic rates
+ * D - function of (y,t,[c]) provides the stochastic rates
+ * Dy - derivative of D in y
+ * parameters - array of parameters to be sent to the functions A,D
+ * the rest should be self-explanatory
+ */
+export function milstein(A: (x: number[], t: number, p: number[]) => number[],
+                            D: (x: number[], t: number, p: number[]) => number[],
+                            Dy: (x: number[], t: number, p: number[]) => number[],
+                            initial: number[], dt: number, t_final: number,
+                            pA: number[], pD: number[]): Solution {
+    var d: number = initial.length;
+    var N: number = Math.floor(t_final / dt);
+    var result = new Float32Array(N * d);
+    var t = new Float32Array(N);
+    var sdt: number = Math.sqrt(dt);
+
+    // initial conditions
+    var i: number, j: number, n: number[];
+    var y_cur: number[] = initial.slice();
+    for (j = 0; j < d; j++) result[j * N] = initial[j];
+    // the grunt-work!
+    for (i = 0; i < N - 1; i++) {
+        var A_cur = A(y_cur, t[i], pA)
+        var D_cur = D(y_cur, t[i], pD)
+        var Dy_cur = Dy(y_cur, t[i], pD)
+        t[i + 1] = (i + 1) * dt;
+        for (j = 0; j < d; j++) {
+            n = boxMuller2();
+            result[i + 1 + j * N] = y_cur[j] =
+                y_cur[j] + dt * A_cur[j] + n[0] * D_cur[j] * sdt
+                - dt / 2 * D_cur[j] * Dy_cur[j] * (1 - n[1] * n[1]);
         }
-        return new Solution(t, result, N);
     }
-    
+    return new Solution(t, result, N);
+}
 
-    /** The Box Muller transform 
-     */
-    export function boxMuller() : number {
-        var v1: number, v2: number, s: number, x: number;
-        do {
-            var u1 = Math.random();
-            var u2 = Math.random();
-            v1 = 2*u1-1;
-            v2 = 2*u2-1;
-            s = v1*v1 +v2*v2;
-        } while (s>1);
+/** Coloured Noise method
+ *
+ * Numerically simulates the Langevin equation,
+ *
+ *      dy/dt = A(y,t) + D(y,t) * noise(t)
+ *
+ * where the noise term is an Ornstein-Uhlenbeck process,
+ * with autocorrelation:
+ *
+ *      <F(t)F(t-tau)> = sigma^2 exp(-|tau|/tau_c) .
+ *
+ * tau_c being the correlation time.
+ *
+ * Because the SDE is differentiable with coloured noise, the
+ * Heun method is used to advance each time-step.
+ *
+ * Arguments:
+ * A - function of (y,t,[c]) provides the non-stochastic rates
+ * D - function of (y,t,[c]) provides the stochastic rates
+ * sigma - standard deviation of gaussian noise
+ * tau - correlation time of the noise
+ * parameters - array of parameters to be sent to the functions A,D
+ * the rest should be self-explanatory
+ */
+export function colour(A: (x: number[], t: number, p: number[]) => number[],
+                        D: (x: number[], t: number, p: number[]) => number[],
+                        sigma: number, tau: number,
+                        initial: number[], dt: number, t_final: number,
+                        pA: number[], pD: number[]): Solution {
+    var d: number = initial.length;
+    var N: number = Math.floor(t_final / dt);
+    var result = new Float32Array(N * d);
+    var t = new Float32Array(N);
+    var n: number = 4; // the most random number
 
-        x = Math.sqrt( -2*Math.log(s)/s)*v1;
-        return x;
+    // coefficients to advance noise
+    var rho: number = Math.exp(-dt / tau);
+    var rhoc: number = sigma * Math.sqrt(1 - rho * rho); // XXX why sigma and not previous
+
+    // Required variables
+    var i: number, j: number;
+    var k1 = new Array(d), k2 = new Array(d);
+    var noiseCur = new Array(d), noiseNext = new Array(d);
+
+    // Initial Conditions
+    var y_cur: number[] = initial.slice();
+    for (j = 0; j < d; j++) {
+        result[j * N] = initial[j];
+        noiseNext[j] = 0;
     }
-    
-    /** Box Muller function that returns both Gaussians
-     */
-    export function boxMuller2() : number[] {
-        var v1, v2, s, x, y;
-        do {
-            var u1 = Math.random();
-            var u2 = Math.random();
-            v1 = 2*u1-1;
-            v2 = 2*u2-1;
-            s = v1*v1 +v2*v2;
-        } while (s>1);
-
-        x = Math.sqrt( -2*Math.log(s)/s)*v1;
-        y = Math.sqrt( -2*Math.log(s)/s)*v2;
-        return [x,y];
+    var y_next = y_cur.slice();
+    for (i = 0; i < N - 1; i++) {
+        // advance the noise and time
+        t[i + 1] = (i + 1) * dt;
+        for (j=0; j<d; j++) {
+            noiseCur[j] = noiseNext[j];
+            n = boxMuller();
+            noiseNext[j] = noiseCur[j] * rho + n * rhoc;
+        }
+        // use heun's method to advance the system
+        var A_cur = A(y_cur, t[i], pA)
+        var D_cur = D(y_cur, t[i], pD)
+        for (j = 0; j < d; j++) {
+            k1[j] = A_cur[j] + D_cur[j] * noiseCur[j];
+            y_next[j] = y_cur[j] + dt * k1[j];
+        }
+        var A_next = A(y_next, t[i], pA)
+        var D_next = D(y_next, t[i], pD)
+        for (j = 0; j < d; j++) {
+            k2[j] = A_next[j] + D_next[j] * noiseNext[j];
+            result[i + 1 + j * N] = y_cur[j] = y_cur[j] + dt / 2 * (k1[j] + k2[j]);
+        }
     }
-    
-    export var gaussian = boxMuller;
+    return new Solution(t, result, N);
+}
 
-};
+
+/** The Box Muller transform 
+ */
+export function boxMuller() : number {
+    var v1: number, v2: number, s: number, x: number;
+    do {
+        var u1 = Math.random();
+        var u2 = Math.random();
+        v1 = 2*u1-1;
+        v2 = 2*u2-1;
+        s = v1*v1 +v2*v2;
+    } while (s>1);
+
+    x = Math.sqrt( -2*Math.log(s)/s)*v1;
+    return x;
+}
+
+/** Box Muller function that returns both Gaussians
+ */
+export function boxMuller2() : number[] {
+    var v1, v2, s, x, y;
+    do {
+        var u1 = Math.random();
+        var u2 = Math.random();
+        v1 = 2*u1-1;
+        v2 = 2*u2-1;
+        s = v1*v1 +v2*v2;
+    } while (s>1);
+
+    x = Math.sqrt( -2*Math.log(s)/s)*v1;
+    y = Math.sqrt( -2*Math.log(s)/s)*v2;
+    return [x,y];
+}
+
+export var gaussian = boxMuller;
